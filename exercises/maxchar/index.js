2fd6db8691dfb2bd0cc5b14fd444415fcd7cae8b
markchernov <mark.chernov@gmail.com>
2019-01-13 13:30:56 -0500

    maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+
+    const count = new Map();
+
+    str.split('').forEach(char => {
+        if (count[char]) {
+            count.set(char, count.get(char) + 1);
+        } else {
+            count.set(char, 1);
+        }
+    });
+
+    let answer = '';
+
+    str.split('').forEach(char => {
+        let highest = 0;
+        if (highest < count.get(char)) {
+            highest = count.get(char)
+            answer = char;
+        }
+    })
+
+    return answer;
+}
 
 module.exports = maxChar;
