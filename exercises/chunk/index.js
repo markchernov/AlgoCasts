ecbe5d4a710038c690e81be9d12c7e318cff6f32
markchernov <mark.chernov@gmail.com>
2019-01-13 15:26:27 -0500

    chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,21 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+
+    const chunks = array.length / size;
+    const answer = [];
+    let start = 0;
+    let end = size;
+
+    for (let i = 0; i < chunks; i++) {
+        const a = array.slice(start, end)
+        answer.push(a);
+        start = start + size;
+        end = start + size;
+    }
+
+    return answer;
+}
 
 module.exports = chunk;
